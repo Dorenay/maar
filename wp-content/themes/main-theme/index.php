f5d885a3ab7a9576f079b4890806535b3fbf7b21
dorenay <dorenay@gmail.com>
2019-01-18 11:49:11 +0200

    after theme add

---
+++ wp-content/themes/main-theme/index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Title</title>
+	<meta name="description" content=""> 
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#000">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+	<style>body { opacity: 0; overflow-x: hidden; } html { background-color: #fff; }</style>
+
+</head>
+
+<body class="background-color">
+	<header class="header-maar24" style="background-image: url('img/BG-first-section.jpg')">
+		<div class="container-md">
+			<div class="header-maar24__top">
+				<div class="header-maar24__burgerMobile">
+					Menu
+					<div class="menu btn2">
+						<div class="icon"></div>
+					</div>
+				</div>
+				<nav class="header-maar24__navigation">
+					<ul>
+						<li><a href="#">Appliance Repair</a></li>               
+						<li><a href="#">Area We Cover</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+				<a href="#" class="header-maar24__logo">
+					<img src="img/logo.png" alt="logo">
+				</a>
+				<div class="header-maar24__phone">
+					<img src="img/icon-phone.png" alt="icon-phone">
+					<a href="tel:+(903) 722 17 03">(903) 722 17 03</a>
+				</div>
+			</div>
+			<div class="header-maar24__context">
+				<h1>Help your appliances  work to the max</h1>
+				<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+			</div>
+		</div>
+	</header>
+	<section class="our-services">
+		<div class="container">
+			<div class="our-services__title">
+				<h2>Our service</h2>
+			</div>
+			<div class="our-services__product">
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+				
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+				<div class="our-services__items">
+					<div class="our-services__items__img">
+						<img src="img/Dishwasher-PNG-Pic.png" alt="img">
+					</div>
+					<div class="our-services__items__title">
+						<h4>Dishwasher Repair</h4>
+					</div>
+					<div class="our-services__hidden">
+						<h4>Dishwasher Repair</h4>
+						<p>Reliable repair partner.  In order to avoid the risk of acciden</p>
+						<button class="our-services__hidden__bt">to order</button>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<div class="container-sn">
+		<section class="form-section ">
+			<div class="container form-section__in">
+				<div class="form-section__title">
+					<h2>Washer Repair Service</h2>
+				</div>
+				<form class="form-section__form">
+					<input type="text" placeholder="Name*">
+					<input type="text" placeholder="Name*">
+					<input type="text" placeholder="Name*">
+					<input type="submit" value="to order">
+				</form>
+			</div>
+		</section>
+	</div>
+
+	<section class="info-sec">
+		<div class="container-sn info-sec__flex">
+			<div class="info-sec__image">
+				<img src="img/KitchenAidLandingPage.jpg" alt="img">
+			</div>
+			<div class="info-sec__text">
+				<div class="info-sec__textContainer">
+					<h3>Alex Johnson</h3>
+					<p>The oven can be gas or electric, perhaps even a combination. Like any home appliances, the oven fails. Mainly, pollution, insects, aging, unstable voltage in the network or disturbance during operation lead to malfunctions. </p>
+					<p>The oven which is built in kitchen furniture or established separately, not for nothing is in demand. The possibility of installing it in any place, separately from the plate, even on a shelf, even on the table, makes use much more appropriate. Accordingly, the number of families in which there is this device is quite large – and the repair of ovens is a very common service.For high-quality oven cleaning, pour a little liquid ammonia into a small container and leave it in a cold oven for the night. In the morning, the dirt easily lags behind the walls of the electric oven. But you need to do it all with an open window, and then evaporation. Don’t forget to wear rubber gloves. The smell isn’t long.
+						The oven can be gas or electric, perhaps even a combination. Like any home appliances, the oven fails. Mainly, pollution, insects, aging, unstable voltage in the network or disturbance during operation lead to malfunctions. </p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="logo-cm">
+		<div class="container">
+			<div class="logo-cm__slider">
+				<div><img src="img/logo-CM-0.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-2.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-3.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-4.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-5.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-0.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-2.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-3.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-4.jpg" alt="img"></div>
+				<div><img src="img/logo-CM-5.jpg" alt="img"></div>
+			</div>
+		</div>
+	</section>
+
+	<section class="contact-maar24">
+		<div class="contact-maar24__content">
+			<div class="contact-maar24__title">
+				<h2>Our contacts</h2>
+			</div>
+			<div class="contact-maar24__inf-text">
+				<p class="minText">Telephone:</p>
+				<a href="tel:+(647) 797-4589" class="bigText">(647) 797-4589</a>
+			</div>
+
+			<div class="contact-maar24__inf-text">
+				<p class="minText">Address:</p>
+				<p class="bigText">451 The West Mall Etobicoke, ON, M9C 1G1</p>
+			</div>
+			<button class="contact-maar24__brn">
+				massege
+			</button>
+			<p class="contact-maar24__copyright">Copyright © 2018. All rights reserved.</p>
+		</div>
+		<div class="contact-maar24__maps">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d10295.239733957973!2d23.98903559855387!3d49.827237390612474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smaps!5e0!3m2!1sru!2sua!4v1547731857057" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</section>
+	
+	<link rel="stylesheet" href="css/main.min.css">
+	<script src="js/scripts.min.js"></script>
+
+</body>
+</html>
